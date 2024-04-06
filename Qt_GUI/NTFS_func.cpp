9a0df7d269f9975fa5b9807d9557ee0e1b28ed60
huy37204 <150261892+huy37204@users.noreply.github.com>
2024-04-06 22:39:27 +0700

    Add files via upload

--- Qt_GUI/NTFS_func.cpp
+++ Qt_GUI/NTFS_func.cpp
@@ -1,9 +1,5 @@
 #include "Header.h"
 
-
-
-
-
 void Computer::NTFS_Read_VBR(int ith_drive, std::wstring drivePath)
 {
     HANDLE hDrive = CreateFile(drivePath.c_str(), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
@@ -32,6 +28,7 @@ void Computer::NTFS_Read_VBR(int ith_drive, std::wstring drivePath)
     std::cout << "Started cluster of extra MFT: " << started_cluster_of_extra_MFT << std::endl;
     std::cout << "Byte per MFT entry: " << byte_per_MFT_entry << std::endl;
     root_Drives[ith_drive]->set_ntfs_vbr(byte_per_sector, sector_per_cluster, sum_sector_of_drive, started_cluster_of_MFT, started_cluster_of_extra_MFT, byte_per_MFT_entry);
+    CloseHandle(hDrive);
 }
 
 
@@ -72,6 +69,7 @@ void Computer::NTFS_Read_MFT(int ith_drive, std::wstring drivePath) {
             if (flag_file_directory_used == 0x01)
             {
                 File* f = new File;
+                f->setAttributes("File");
                 Header_MFT_Entry mft_header;
                 mft_header.started_attribute_offset = mft[0x14] | mft[0x15] << 8;
                 mft_header.flag = mft[0x16] | mft[0x17] << 8;
@@ -186,6 +184,7 @@ void Computer::NTFS_Read_MFT(int ith_drive, std::wstring drivePath) {
             else if (flag_file_directory_used == 0x03)
             {
                 Directory* f = new Directory;
+                f->setAttributes("Folder");
                 Header_MFT_Entry mft_header;
                 mft_header.started_attribute_offset = mft[0x14] | mft[0x15] << 8;
                 mft_header.flag = mft[0x16] | mft[0x17] << 8;
@@ -340,6 +339,7 @@ void File::NTFS_Nonresident_Read_Data(std::wstring drivePath, NTFS_VBR vbr)
         nonresidentinfo.datasize -= (long long)(vbr.byte_per_sector * vbr.sector_per_cluster);
         delete[] DATA;
     }
+    CloseHandle(hDrive);
 }
 
 int NTFS_Read_BITMAP(BYTE* attr, Header_Attribute h)
@@ -362,16 +362,15 @@ void NTFS_Create_Date_Time(BYTE* attr, Header_Attribute h, Date& d, Time& t)
     for (int i = 0; i < 8; ++i) {
         hexValue |= static_cast<unsigned long long>(data_attr[i]) << (i * 8);
     }
-
     unsigned long long nanoSeconds = hexValue * 100;
 
-    long long epochTime1970 = 11644473600LL;
+    long long epochTime1970 = 11644473600ULL;
     long long secondsSince1970 = static_cast<long long>((nanoSeconds) / 1000000000LL) - epochTime1970;
 
     time_t timeSince1970 = static_cast<time_t>(secondsSince1970);
 
     struct tm timeinfo;
-    gmtime_s(&timeinfo, &timeSince1970);
+    localtime_s(&timeinfo, &timeSince1970);
 
     d.day = timeinfo.tm_mday;
     d.month = timeinfo.tm_mon + 1;
@@ -380,10 +379,7 @@ void NTFS_Create_Date_Time(BYTE* attr, Header_Attribute h, Date& d, Time& t)
     t.minute = timeinfo.tm_min;
     t.second = timeinfo.tm_sec;
     delete[] data_attr;
-    //cout << "Month " << timeinfo.tm_mon + 1 << ", ";
-    //cout << "Date " << timeinfo.tm_mday << ", ";
-    //cout << "Year " << timeinfo.tm_year + 1900 << ", ";
-    //cout << "Time " << timeinfo.tm_hour << ":" << timeinfo.tm_min << ":" << timeinfo.tm_sec << endl;
+
 }
 
 std::string NTFS_Create_Name(BYTE* attr, Header_Attribute h)
@@ -404,14 +400,15 @@ std::string NTFS_Create_Name(BYTE* attr, Header_Attribute h)
 
 Directory* Drive::NTFS_Find_Parent_Directory(int parent_id)
 {
-
     for (int i = 0; i < rootDirectories_Files.size(); i++)
     {
         if (dynamic_cast<Directory*>(rootDirectories_Files[i]))
         {
             if (static_cast<Directory*>(rootDirectories_Files[i])->NTFS_Get_ID() == parent_id)
                 return static_cast<Directory*>(rootDirectories_Files[i]);
-            return static_cast<Directory*>(rootDirectories_Files[i])->NTFS_Find_Parent_Directory(parent_id);
+            Directory* d = static_cast<Directory*>(rootDirectories_Files[i])->NTFS_Find_Parent_Directory(parent_id);
+            if (d)
+                return d;
         }
     }
     return NULL;
